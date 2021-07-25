c8e695d5e1d35f039aeb66f8e5a95ec4840cfe0e
IdanLnrd <idan@lnrd.io>
2021-07-25 17:22:20 +0300

    init survey js

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { SurveyWindow } from 'survey-react';
 
 function App() {
+  const surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
+    { name:"page1", questions: [ 
+        { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
+        { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{frameworkUsing} = 'Yes'" }
+     ]},
+    { name: "page2", questions: [
+      { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
+      { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
+    { name: "page3",questions: [
+      { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
+   ]
+  }
+  const sendDataToServer = data => {
+    console.log('sendDataToServer: ', data);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Good Learn React
-        </a>
+        Survey
       </header>
+      <div>
+        <SurveyWindow 
+          json={surveyJSON} 
+          onComplete={sendDataToServer}>
+        </SurveyWindow>
+        
+      </div>
     </div>
   );
 }
