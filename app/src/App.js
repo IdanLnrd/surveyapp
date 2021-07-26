49caf130500e38060ec3bcf4caf2debffb6dfa4c
IdanLnrd <idan@lnrd.io>
2021-07-26 11:40:58 +0300

    ref

--- app/src/App.js
+++ app/src/App.js
@@ -1,33 +1,74 @@
 import './App.css';
 import React from 'react';
-import { SurveyWindow } from 'survey-react';
-
+import * as Survey from 'survey-react';
 function App() {
-  const surveyJSON = { title: "Tell us, what technologies do you use?", pages: [
-    { name:"page1", questions: [ 
-        { type: "radiogroup", choices: [ "Yes", "No" ], isRequired: true, name: "frameworkUsing",title: "Do you use any front-end framework like Bootstrap?" },
-        { type: "checkbox", choices: ["Bootstrap","Foundation"], hasOther: true, isRequired: true, name: "framework", title: "What front-end framework do you use?", visibleIf: "{frameworkUsing} = 'Yes'" }
-     ]},
-    { name: "page2", questions: [
-      { type: "radiogroup", choices: ["Yes","No"],isRequired: true, name: "mvvmUsing", title: "Do you use any MVVM framework?" },
-      { type: "checkbox", choices: [ "AngularJS", "KnockoutJS", "React" ], hasOther: true, isRequired: true, name: "mvvm", title: "What MVVM framework do you use?", visibleIf: "{mvvmUsing} = 'Yes'" } ] },
-    { name: "page3",questions: [
-      { type: "comment", name: "about", title: "Please tell us about your main requirements for Survey library" } ] }
-   ]
+  
+  const surveyJSON = {
+    title: "Tell us, what technologies do you use?",
+    pages: [
+      {
+        name: "page1", questions: [
+          { 
+            type: "radiogroup", 
+            choices: ["Yes", "No"], 
+            isRequired: true, 
+            name: "frameworkUsing", 
+            title: "Do you use any front-end framework like Bootstrap?" 
+          },
+          { 
+            type: "checkbox", 
+            choices: ["Bootstrap", "Foundation"], 
+            hasOther: true, isRequired: true, 
+            name: "framework", 
+            title: "What front-end framework do you use?", 
+            visibleIf: "{frameworkUsing} = 'Yes'" }
+        ]
+      },
+      {
+        name: "page2", questions: [
+          { 
+            type: "radiogroup", 
+            choices: ["Yes", "No"], 
+            isRequired: true, 
+            name: "mvvmUsing", 
+            title: "Do you use any MVVM framework?" },
+          { 
+            type: "checkbox", 
+            choices: ["AngularJS", "KnockoutJS", "React"], 
+            hasOther: true, 
+            isRequired: true, 
+            ame: "mvvm", 
+            title: "What MVVM framework do you use?", 
+            visibleIf: "{mvvmUsing} = 'Yes'" }]
+      },
+      {
+        name: "page3", questions: [
+          { 
+            type: "comment", 
+            name: "about", 
+            title: "Please tell us about your main requirements for Survey library" 
+          }
+        ]
+      }
+    ]
   }
+  
   const sendDataToServer = data => {
-    console.log('sendDataToServer: ', data);
+    console.log('sendDataToServer: ', data.valuesHash);
   }
+  // var surveyCreator = new SurveyCreator.SurveyCreator("surveyCreatorDivElementID");
+  console.log('surveyCreator:', Survey.SurveyCreator);
   return (
     <div className="App">
       <header className="App-header">
-        Survey
+        <div style={{textAlign: 'start', fontSize: '16px'}}><pre>{JSON.stringify(surveyJSON, null, 2)}</pre></div>
       </header>
       <div>
-        <SurveyWindow 
-          json={surveyJSON} 
+        <div id="surveyCreatorDivElementID"></div>
+        <Survey.SurveyWindow 
+          json={surveyJSON}
           onComplete={sendDataToServer}>
-        </SurveyWindow>
+        </Survey.SurveyWindow>
         
       </div>
     </div>
